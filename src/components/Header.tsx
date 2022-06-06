b3e693ffab173af32551facad6af59fa09523a4c
Lucas Cardoso <lucao@MacBook-Air-de-Elika.local>
2022-06-06 18:36:07 -0400

    mobile menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,32 @@
-import {Box, Flex, HStack, Img, Link} from '@chakra-ui/react'
+import {Box, Flex, HStack, Icon, IconButton, Img, Link, Menu, MenuButton, MenuItem, MenuList} from '@chakra-ui/react'
 import { ActiveLink } from './ActiveLink'
+import { useState } from 'react'
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
 
 export function Header(){
 
+    const [openMenu, setOpenMenu] = useState(false)
+
     return(
-        <Box bg='black' w="100%" border="1px" borderBottom="gray.900" style={{
-            position: 'sticky',
+        <Box bg='black' w="100%" border="1px" borderBottom="gray.900" position={["absolute", "sticky"]} style={{
             top: 0,
-            zIndex: 20
+            zIndex: 30
         }}>
-            <Flex maxW="1120px" m="0 auto" h="120px" align="center" justify="space-between" p="20px">
-                <Img src="/logo.png" h="58px"/>
-                <HStack spacing={10}>
+            <Flex
+                bg="black"
+                maxW="1120px"
+                w={["100%", "initial"]}
+                m="0 auto"
+                h={["100vh","120px"]}
+                align="center"
+                justify={["center","space-between"]}
+                p="20px"
+                display={[openMenu ? "flex" : "none", "flex"]}
+                direction={["column", "row"]}
+                position={["fixed", "initial"]}
+            >
+                <Img src="/logo.png" h="58px" mb={["1.5rem", "0"]}/>
+                <Flex display="flex" direction={["column", null, "row"]} gap="40px" textAlign={["center", "initial"]}>
                     <ActiveLink href="#home">
                         <Link>Início</Link>
                     </ActiveLink>
@@ -47,7 +62,11 @@ export function Header(){
                     <ActiveLink href="#location">
                         <Link>Localização</Link>
                     </ActiveLink>
-                </HStack>
+                </Flex>
+            </Flex>
+
+            <Flex position="absolute" right="1.5rem" top="1.5rem" onClick={() => setOpenMenu(!openMenu)}>
+                <Icon as={!openMenu ? AiOutlineMenu : AiOutlineClose} color="white" fontSize="2rem" fontWeight="light"/>
             </Flex>
         </Box>
     )
