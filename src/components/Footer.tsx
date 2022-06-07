6d5e8051dd22344788c31231b4a1edff75fe016f
Lucas Cardoso <lucao@MacBook-Air-de-Elika.local>
2022-06-06 21:30:49 -0400

    footer responsivity

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,10 +8,18 @@ export function Footer(){
 
     return(
         <Box borderTop="1px" borderTopColor="gray.900" w="100%">
-            <Flex color="white" align="center" w="100%" maxW="1500px" p="10px" mx="auto">
+            <Flex 
+            color="white" 
+            align="center" 
+            w="100%" 
+            maxW="1500px" 
+            p="20px"
+            mx="auto" 
+            direction={{sm: "column", lg: "row"}} 
+            gap={{sm: "40px", lg: "0px"}}>
                 <Text flex="3" textAlign="center">13.425.720/0001-40 | Igreja Evangelica Fonte Viva</Text>
                 <Img src="/logo.png" h="100px" w="100px" mx="8rem"/>
-                <SimpleGrid columns={4} spacing={10} flex="3" fontSize="2rem">
+                <SimpleGrid columns={4} spacing={{sm: 100, lg: 10}} flex="3" fontSize="2rem">
 
                     <FooterIcon icon={BsYoutube} link=""/>
 
