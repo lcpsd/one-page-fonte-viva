3e41ca76c39924e929e327c3c5e3ef937fffba51
Lucas Cardoso <lucao@MacBook-Air-de-Elika.local>
2022-06-08 13:37:45 -0400

    footer links update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,36 +1,45 @@
 import { Box, Flex, Icon, Img, Link, SimpleGrid, Text } from "@chakra-ui/react";
 import { AiFillInstagram } from "react-icons/ai";
-import { BsFacebook, BsYoutube } from "react-icons/bs";
+import { BsFacebook, BsSpotify, BsYoutube } from "react-icons/bs";
 import { RiWhatsappFill } from "react-icons/ri";
 import { FooterIcon } from "./FooterIcon";
+import {IoLogoAppleAppstore, IoLogoGooglePlaystore} from 'react-icons/io5'
 
 export function Footer(){
 
     return(
-        <Box borderTop="1px" borderTopColor="gray.900" w="100%">
+        <Flex borderTop="1px" borderTopColor="gray.900" w="100%" direction="column" align="center" p="10px">
             <Flex 
             color="white" 
             align="center" 
             w="100%" 
             maxW="1500px" 
-            p="20px"
+            p="10px"
             mx="auto" 
             direction={{base: "column", lg: "row"}} 
             gap={{base: "40px", lg: "0px"}}>
                 <Text flex="3" textAlign="center">13.425.720/0001-40 | Igreja Evangelica Fonte Viva</Text>
                 <Img src="/logo.png" h="100px" w="100px" mx="8rem"/>
-                <SimpleGrid columns={4} spacing={{base: 100, lg: 10}} flex="3" fontSize="2rem">
+                <SimpleGrid columns={7} spacing={{base: 100, lg: 10}} flex="3" fontSize="2rem">
 
-                    <FooterIcon icon={BsYoutube} link=""/>
+                    <FooterIcon icon={BsYoutube} link="https://www.youtube.com/c/IgrejaFonteViva"/>
 
-                    <FooterIcon icon={AiFillInstagram} link=""/>
+                    <FooterIcon icon={AiFillInstagram} link="https://www.instagram.com/igrejafontevivaonline/"/>
 
-                    <FooterIcon icon={BsFacebook} link=""/>
+                    <FooterIcon icon={BsFacebook} link="https://www.facebook.com/IgrejaFonteViva"/>
 
-                    <FooterIcon icon={RiWhatsappFill} link=""/>
+                    <FooterIcon icon={BsSpotify} link="https://open.spotify.com/show/4udkQ3vEpq9quj9Wcnd2s9?si=2749b33bbd2f4761"/>
+
+                    <FooterIcon icon={RiWhatsappFill} link="https://api.whatsapp.com/send?phone=5567992221076&text=Oi"/>
+
+                    <FooterIcon icon={IoLogoAppleAppstore} link="https://apps.apple.com/br/app/igreja-fonte-viva/id1476545344"/>
+
+                    <FooterIcon icon={IoLogoGooglePlaystore} link="https://play.google.com/store/apps/details?id=com.inpeace.igrejafonteviva.campogrande"/>
 
                 </SimpleGrid>
             </Flex>
-        </Box>
+
+            <Text color="gray.600">© 2011 – {new Date().getFullYear()} Igreja Fonte Viva Todos os Direitos Reservados.</Text>
+        </Flex>
     )
 }
