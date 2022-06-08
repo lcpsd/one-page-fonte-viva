1a7760bf21bc3ba92d73cb982929c3366308861a
Lucas Cardoso <lucao@MacBook-Air-de-Elika.local>
2022-06-08 14:24:24 -0400

    documents sectiomn

---
+++ src/components/DocumentsSection.tsx
@@ -0,0 +1,42 @@
+import { SimpleGrid, Text } from "@chakra-ui/react";
+import { Section } from "./current-section";
+import { DocumentCard } from "./DocumentCard";
+
+export function DocumentsSection(){
+
+    const documents = [
+        {
+          id: 1,
+          title: "Casa Viva",
+          link:""
+        },
+        {
+          id: 2,
+          title: "Batismo",
+          link:""
+        },
+        {
+          id: 3,
+          title: "Visão",
+          link:""
+        },
+        {
+          id: 4,
+          title: "eBooks",
+          link:""
+        },
+    ]
+
+    return(
+        <Section id="documents" h="90vh" w="100%" maxW="1500px" m="0 auto" direction="column" justify="center" align="center" px="40px">
+          <Text fontSize="2xl" fontWeight="bold" color="blue.500" textAlign="center" mb="1rem">Documentos</Text>
+          <SimpleGrid columns={{base: 1, lg: 4}} spacing={10}>
+            {
+              documents.map(document => (
+                <DocumentCard data={document}/>
+              ))
+            }
+          </SimpleGrid>
+        </Section>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,29 +13,6 @@ import { HeaderSection } from "../components/HeaderSection";
 
 export default function Home() {
 
-  const documents = [
-    {
-      id: 1,
-      title: "Casa Viva",
-      link:""
-    },
-    {
-      id: 2,
-      title: "Batismo",
-      link:""
-    },
-    {
-      id: 3,
-      title: "Visão",
-      link:""
-    },
-    {
-      id: 4,
-      title: "eBooks",
-      link:""
-    },
-  ]
-
   const financial = [
     {
       id: 1,
@@ -72,18 +49,7 @@ export default function Home() {
         <PastorsCarousel />
 
         <HoursSection />
-        
-        {/* Documents */}
-        <Section id="documents" h="90vh" w="100%" maxW="1500px" m="0 auto" direction="column" justify="center" align="center" px="40px">
-          <Text fontSize="2xl" fontWeight="bold" color="blue.500" textAlign="center" mb="1rem">Documentos</Text>
-          <SimpleGrid columns={{base: 1, lg: 4}} spacing={10}>
-            {
-              documents.map(document => (
-                <DocumentCard data={document}/>
-              ))
-            }
-          </SimpleGrid>
-        </Section>
+    
         
         {/* Financial */}
         <Section id="financial" h="90vh" w="100%" maxW="1200px" m="0 auto" direction="column" justify="center" align="center" px="40px">
