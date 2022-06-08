41f645d4a9d63eff8c5507b5d5917317449f4762
Lucas Cardoso <lucao@MacBook-Air-de-Elika.local>
2022-06-08 14:26:18 -0400

    financial section component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,29 +10,9 @@ import { HoursSection } from "../components/HoursSection";
 import { VisionSection } from "../components/VisionSection";
 import { HistorySection } from "../components/HistorySection";
 import { HeaderSection } from "../components/HeaderSection";
+import { FinancialSection } from "../components/FinancialSection";
 
 export default function Home() {
-
-  const financial = [
-    {
-      id: 1,
-      title:"PIX",
-      description:"ipsc4c917fb-17df-4f5b-88d4-6a3345c4a31d",
-      image_link: "/images/pix-logo.png"
-    },
-    {
-      id: 2,
-      title:"Bradesco",
-      description:"AG: 073\nCC: 677-7",
-      image_link: "/images/bradesco-logo.jpg"
-    },
-    {
-      id: 3,
-      title:"Caixa Econômica",
-      description:"AG: 3658\nOP: 003\nCC: 34-6",
-      image_link: "/images/caixa-logo.png"
-    },
-  ]
 
   return (
     <>
@@ -50,19 +30,8 @@ export default function Home() {
 
         <HoursSection />
     
-        
-        {/* Financial */}
-        <Section id="financial" h="90vh" w="100%" maxW="1200px" m="0 auto" direction="column" justify="center" align="center" px="40px">
-          <Text fontSize="2xl" fontWeight="bold" color="blue.500" textAlign="center" mb="1rem">Financeiro</Text>
-          <SimpleGrid columns={{base: 1, lg: 3}} spacing={10} w="100%">
-            {
-              financial.map(data => (
-                <FinancialCard data={data} copy/>
-              ))
-            }
-          </SimpleGrid>
-        </Section>
-        
+        <FinancialSection />
+
         {/* Location */}
         <Section id="location" h="100%" minH="75vh" w="100%" maxW="1200px" m="0 auto" justify="center" align="center" p="40px">
           <SimpleGrid spacing={10} columns={{base: 1, lg: 2}}>
