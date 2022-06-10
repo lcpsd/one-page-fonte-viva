803c5e3fa3d3cd18ee7fdee843d8ce7e55d7ed1f
Lucas Cardoso <lucao@MacBook-Air-de-Elika.local>
2022-06-10 12:34:24 -0400

    mobile menu animation improved

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,8 +11,8 @@ export function Header(){
     const isLarge = useIsLarge()
 
     const variants = isLarge ? {
-        visible:{opacity: 1, transition: {ease: "easeIn", duration: 0.3}},
-        hidden: {opacity: 0, transition: {ease: "easeOut", duration: 0.3}}
+        visible:{opacity: 1, zIndex: 10, transition: {ease: "easeIn", duration: 0.3}},
+        hidden: {opacity: 0, left: "-100rem", transition: {ease: "easeOut", duration: 0.3}}
     } : {
         visible:{opacity: 1},
         hidden: {opacity: 1},
@@ -38,7 +38,7 @@ export function Header(){
                 align="center"
                 justify={{base:"center", lg:"space-between"}}
                 p="20px"
-                display={openMenu ? "flex" : {base: "none", lg: "flex"}}
+                display="flex"
                 direction={{base:"column", lg:"row"}}
                 position={{base:"fixed", lg:"initial"}}
                 as={motion.div}
@@ -101,7 +101,9 @@ export function Header(){
               top="0"
               p="10px"
               align="center"
-              justify={!openMenu ? "space-between": "right"}>
+              justify={!openMenu ? "space-between": "right"}
+              zIndex={10}
+              >
                 <Img src="/logo.png" h="50px" opacity={0.3} display={!openMenu ? "block": "none"}/>
                 <Icon as={!openMenu ? AiOutlineMenu : AiOutlineClose} color="white" fontSize="2rem" fontWeight="light"/>
             </Flex>
