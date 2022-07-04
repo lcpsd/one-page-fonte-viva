978f50bd602b78c54dd3295e9cefd97a68931165
Lucas <dev@rnamob002.rnatec.com.br>
2022-07-04 18:51:51 -0400

    feat: location map link

--- src/components/sections/LocationSection.tsx
+++ src/components/sections/LocationSection.tsx
@@ -1,11 +1,14 @@
 import { Flex, Img, SimpleGrid, Text } from "@chakra-ui/react";
+import Link from "next/link";
 import { Section } from "../current-section";
 
 export function LocationSection(){
     return(
         <Section id="location" h="100%" minH="75vh" w="100%" maxW="1200px" m="0 auto" justify="center" align="center" p="40px">
           <SimpleGrid spacing={10} columns={{base: 1, lg: 2}}>
-            <Img src="/images/map.png"/>
+            <Link href='https://goo.gl/maps/aNWCwjcJxSyVTboX7'>
+              <Img src="/images/map.png" cursor='pointer'/>
+            </Link>
             <Flex color="white" justify="center" direction="column">
               <Text fontSize="2xl" fontWeight="bold" color="blue.500" textAlign={{base:"center", lg: "left"}}>Visite-nos</Text>
               <Text textAlign={{base:"center", lg: "left"}}>
