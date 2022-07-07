7f8b1e05c533ffa0fe63fc88c9a9dd9960774305
Lucas <dev@rnamob002.rnatec.com.br>
2022-07-07 15:22:26 -0400

    feat: app section responsivity

--- src/components/sections/AppSection.tsx
+++ src/components/sections/AppSection.tsx
@@ -1,4 +1,5 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Img, Text } from "@chakra-ui/react";
+import Link from "next/link";
 import { Section } from "../current-section";
 
 export function AppSection(){
@@ -8,11 +9,13 @@ export function AppSection(){
         id='app'
         h='100vh'
         w='100%'
-        maxW='1500px'
+        maxW='1000px'
         align='center'
         justify='center'
         mx='auto'
+        direction={{base: 'column', xl:'row'}}
         >
+            {/* Smartphone */}
             <Flex flex='1' justify='center'>
                 <Box
                 borderRadius='10px'
@@ -20,6 +23,7 @@ export function AppSection(){
                 borderColor='blue.500'
                 p='10px'
                 position='relative'
+                mb={{base: '10rem', xl:'0'}}
                 >
                     <Box
                     position='absolute'
@@ -48,8 +52,21 @@ export function AppSection(){
                 </Box>
             </Flex>
 
-            <Flex flex='1'>
 
+            <Flex flex='3' align='center'>
+                <Flex direction='column' align={{base:'center', xl:'flex-end'}}>
+                    <Text fontSize={{base: '60px', xl:'96px'}} fontWeight='bold' color='white' lineHeight='10px'>APLICATIVO</Text>
+                    <Text fontSize={{base: '60px', xl:'96px'}} fontWeight='bold' color='blue.500'>FONTE VIVA</Text>
+                    <Text color='white' fontSize='24px' w={{base:'100%', xl:'60%'}} mt={{base:'0rem', xl:'-2rem'}} mb='1rem' textAlign={{base:'center', xl:'right'}}>Desenvolvido para estretiar nossa comunhão e organizar a instituição</Text>
+                    <Flex>
+                        <Link href='https://apps.apple.com/br/app/igreja-fonte-viva/id1476545344'>
+                            <Img src="/images/app-store-button.png" h='60px' mr='10px'/>
+                        </Link>
+                        <Link href='https://play.google.com/store/apps/details?id=com.inpeace.igrejafonteviva.campogrande'>
+                            <Img src="/images/google-play-button.png" h='60px'/>
+                        </Link>
+                    </Flex>
+                </Flex>
             </Flex>
         </Section>
     )
