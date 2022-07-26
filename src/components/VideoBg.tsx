8e7bb63c4c7ebe0ecda9953391977bea396acaa5
Lucas <dev@rnamob002.rnatec.com.br>
2022-07-26 11:40:29 -0400

    fix: video bg data loading in apple systems

--- src/components/VideoBg.tsx
+++ src/components/VideoBg.tsx
@@ -8,11 +8,9 @@ export function VideoBg({source, ...rest}: VideoBgProps){
 
     return(
         <Flex w='100%' h='100%' justify='center' align='center' {...rest} objectFit='cover'>
-            {
-                <video  autoPlay muted loop style={{height: '140%', width: 'auto', objectFit: 'cover'}} id='video'>
-                    <source src={source} type='video/mp4'/>
-                </video>
-            }
+            <video preload="auto" controls autoPlay muted loop playsInline style={{height: '140%', width: 'auto', objectFit: 'cover'}} id='video'>
+                <source src={source} type='video/mp4'/>
+            </video>
         </Flex>
     )
 }
--- src/components/sections/HeaderSection.tsx
+++ src/components/sections/HeaderSection.tsx
@@ -63,7 +63,7 @@ export function HeaderSection(){
              opacity={{base:'0.5', xl:'1'}}
              >
             <GradientDiv side='to-r' zIndex='10'/>
-            <VideoBg source='/video/bg-header-video.mp4' opacity={{base:'0.5', xl:'1'}} objectFit='cover'/>
+            <VideoBg source='https://i.imgur.com/uiYcF6n.mp4' opacity={{base:'0.5', xl:'1'}} objectFit='cover'/>
            </Flex>
         </Section>
     )
